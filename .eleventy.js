c3d835ea41b5cbc377ca8c6a57325841062dbad6
chalatz <chralatz@gmail.com>
2026-05-27 16:41:04 +0300

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+// The export statement makes these settings available to other files in 11ty
+module.exports = function(eleventyConfig) {
+  // Watch SCSS for changes
+  eleventyConfig.addWatchTarget("./assets/scss/**/*.scss");
+  
+  // Passthrough copy
+  eleventyConfig.addPassthroughCopy("assets/css/styles.css");
+  eleventyConfig.addPassthroughCopy("assets/js/scripts.js");
+  eleventyConfig.addPassthroughCopy("assets/images/**/*");
+};
