39d10c85a080a2a0b3e6a124f9b51c4618c78ffe
chalatz <chralatz@gmail.com>
2026-05-27 18:00:56 +0300

    Initialize sliders

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,7 @@
     const sliders = document.querySelectorAll('.slider');
 
     sliders.forEach(slider => {
+        slider.value = 0;
         slider.addEventListener('input', () => {
 
             document.querySelector(`.index--${slider.dataset.target}`).innerHTML = slider.value;
