af9e31d3a9802d096dec0987618e58d8af082462
chalatz <chralatz@gmail.com>
2026-05-27 17:52:05 +0300

    logistics done

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+(function() {
+   
+    const sliders = document.querySelectorAll('.slider');
+
+    sliders.forEach(slider => {
+        slider.addEventListener('input', () => {
+
+            document.querySelector(`.index--${slider.dataset.target}`).innerHTML = slider.value;
+
+            document.documentElement.style.setProperty(`--${slider.dataset.target}`, slider.value);
+
+            
+        });
+    });
+
+})();
